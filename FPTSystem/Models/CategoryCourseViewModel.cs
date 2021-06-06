e09a63ca43bfc876907530760b114fdbbd1c3d2d
Tran Quang Duy <62538774+duytran167@users.noreply.github.com>
2021-06-06 15:00:05 +0700

    Update Staff/Trainer

---
+++ FPTSystem/Models/CategoryCourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FPTSystem.Models
+{
+	public class CategoryCourseViewModel
+	{
+		public Course Course { get; set; }
+		public int Id { get; set; }
+		public IEnumerable<Category> Categories { get; set; }
+	}
+}
