82d2a25d9fdf7fe1511fc0e172ebe50830942c7f
Tran Quang Duy <62538774+duytran167@users.noreply.github.com>
2021-06-08 11:44:43 +0700

    Create Account for Trainee/Staff

---
+++ FPTSystem/Controllers/StaffController.cs
@@ -0,0 +1,32 @@
+﻿using FPTSystem.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPTSystem.Controllers
+{
+  [Authorize(Roles ="staff")]
+    public class StaffController : Controller
+
+    {
+    private ApplicationUser _user;
+    private ApplicationDbContext _context;
+    private UserManager<ApplicationUser> _usermanager;
+    public StaffController()
+    {
+      _user = new ApplicationUser();
+      _context = new ApplicationDbContext();
+      _usermanager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+    }
+    // GET: Staff
+
+    public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FPTSystem/Controllers/TraineeController.cs
@@ -0,0 +1,33 @@
+﻿using FPTSystem.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPTSystem.Controllers
+{
+  [Authorize(Roles = "trainee")]
+    public class TraineeController : Controller
+    {
+    private ApplicationUser _user;
+    private ApplicationDbContext _context;
+    private UserManager<ApplicationUser> _usermanager;
+     public TraineeController()
+        {
+            _user = new ApplicationUser();
+            _context = new ApplicationDbContext();
+            _usermanager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+        }
+
+        // GET: Trainee
+        public ActionResult Index()
+        {
+      var ftraineeId = User.Identity.GetUserId();
+      var trainee = _context.Users.OfType<Trainee>().SingleOrDefault(t => t.Id == ftraineeId);
+      return View(trainee);
+    }
+    }
+}
